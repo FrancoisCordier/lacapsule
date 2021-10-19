cad5e8f73b1e40bf241d6f3ccd4488a016b4291e
François Cordier <francoiscordier.ece@gmail.com>
2021-10-19 16:06:55 +0200

    Update

--- challenges/techlevel/routes/index.js
+++ challenges/techlevel/routes/index.js
@@ -6,37 +6,37 @@ const devPersons = [
     name: "Leanne Graham",
     sex: "female",
     skills: ["HTML", "CSS"],
-    level: "75",
+    level: 75,
   },
   {
     name: "Ervin Howell",
     sex: "male",
     skills: ["HTML", "NodeJS", "Express"],
-    level: "30",
+    level: 30,
   },
   {
     name: "Clementine Bauch",
     sex: "female",
     skills: ["HTML", "CSS"],
-    level: "49",
+    level: 49,
   },
   {
     name: "Kurtis Weissnat",
     sex: "male",
     skills: ["JavaScript", "jQuery", "NodeJS", "Express"],
-    level: "67",
+    level: 67,
   },
   {
     name: "Chelsey Dietrich",
     sex: "female",
     skills: ["HTML", "CSS", "JavaScript", "NodeJS", "Express"],
-    level: "96",
+    level: 96,
   },
   {
     name: "Dennis Schulist",
     sex: "male",
     skills: ["HTML", "CSS", "JavaScript"],
-    level: "54",
+    level: 54,
   },
 ];
 
